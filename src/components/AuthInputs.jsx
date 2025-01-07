d36e6ffbce457a527708d560d61f40932f5f18bb
cmnishant <cmnishantwork@gmail.com>
2025-01-07 23:31:14 +0530

    Update the file for props

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -6,7 +6,8 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
+
 const Label = styled.label`
   display: block;
   margin-bottom: 0.5rem;
@@ -14,18 +15,19 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
-`
+  color: ${(invalid) => invalid ? '#f87171' : '#6b7280' };
+`;
+
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
   background-color: #d1d5db;
-  color: #374151;
+  color:${({value}) => (value ? '#ef4444' : '#374151')};
   border: 1px solid transparent;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -51,15 +53,12 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className="paragraph">
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>
-            Email
-          </Label>
+          <Label invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
             // }}
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
