c4cb73c43e59138e77b02c1b3d381c0e0e701e17
cmnishant <cmnishantwork@gmail.com>
2025-01-08 23:51:27 +0530

    Update the file header components style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { styled } from 'styled-components';
+
 import logo from '../assets/logo.png';
 import classes from './Header.module.css';
 
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`;
+
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p className={classes.paragraph}>A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeader>
   );
 }
